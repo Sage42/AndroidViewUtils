d678cba62d6f83e4fb672d9df439056d4401944b
Teo Yong Kai <teoyongkai@Teos-MacBook-Air.local>
2014-05-16 18:23:16 +0800

    Refactor ExpandAndShrinkCardView to accept different layout wrapping

--- samples/src/main/java/com/sage42/android/view_samples/ui/SampleExpandShrinkListAdapter.java
+++ samples/src/main/java/com/sage42/android/view_samples/ui/SampleExpandShrinkListAdapter.java
@@ -3,6 +3,7 @@ package com.sage42.android.view_samples.ui;
 import android.content.Context;
 import android.database.Cursor;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -11,48 +12,57 @@ import com.sage42.android.view.ui.ExpandAndShrinkCardView;
 import com.sage42.android.view.ui.ExpandAndShrinkCardsListViewAdapter;
 import com.sage42.android.view_samples.R;
 
-public class SampleExpandShrinkListAdapter extends ExpandAndShrinkCardsListViewAdapter<SampleExpandAndShrinkCard>
-{
+public class SampleExpandShrinkListAdapter extends
+		ExpandAndShrinkCardsListViewAdapter {
 
-    public SampleExpandShrinkListAdapter(final Context context, final Cursor cursor)
-    {
-        super(context, cursor);
-    }
+	public SampleExpandShrinkListAdapter(final Context context,
+			final Cursor cursor) {
+		super(context, cursor);
+	}
 
-    static class ViewHolder
-    {
-        public TextView title;
-        public TextView text;
-    }
+	static class ViewHolder {
+		public TextView title;
+		public TextView text;
+	}
 
-    @Override
-    public void bindExpandAndShrinkView(final ExpandAndShrinkCardView view, final Context context, final Cursor cursor)
-    {
-        if (view instanceof SampleExpandAndShrinkCard)
-        {
-            final SampleExpandAndShrinkCard sampleCard = (SampleExpandAndShrinkCard) view;
-            sampleCard.bind(cursor);
-        }
-        return;
-    }
+	@Override
+	public SampleExpandAndShrinkCard newExpandAndShrinkView(
+			final Context context, final Cursor cursor, final ViewGroup parent) {
+		final LayoutInflater inflater = LayoutInflater.from(this.mContext);
+		final SampleExpandAndShrinkCard rootView = (SampleExpandAndShrinkCard) inflater
+				.inflate(R.layout.sample_expand_shrink_card_view, parent, false);
 
-    @Override
-    public SampleExpandAndShrinkCard newExpandAndShrinkView(final Context context, final Cursor cursor,
-                    final ViewGroup parent)
-    {
-        final LayoutInflater inflater = LayoutInflater.from(this.mContext);
-        final SampleExpandAndShrinkCard rootView = (SampleExpandAndShrinkCard) inflater.inflate(
-                        R.layout.sample_expand_shrink_card_view, parent, false);
-        final ClickToOpenCloseLayout containerView = (ClickToOpenCloseLayout) rootView
-                        .findViewById(R.id.main_container);
-        rootView.setParentContainer(containerView);
+		// Setup ViewHolder
+		final ViewHolder viewHolder = new ViewHolder();
+		viewHolder.title = (TextView) rootView.findViewById(R.id.card_title);
+		viewHolder.text = (TextView) rootView.findViewById(R.id.card_text);
+		rootView.setTag(viewHolder);
 
-        // Setup ViewHolder
-        final ViewHolder viewHolder = new ViewHolder();
-        viewHolder.title = (TextView) rootView.findViewById(R.id.card_title);
-        viewHolder.text = (TextView) rootView.findViewById(R.id.card_text);
-        rootView.setTag(viewHolder);
+		return rootView;
+	}
 
-        return rootView;
-    }
+	@Override
+	public void bindExpandAndShrinkView(View view, Context arg1, Cursor cursor) {
+
+		if (view instanceof SampleExpandAndShrinkCard) {
+			final SampleExpandAndShrinkCard sampleCard = (SampleExpandAndShrinkCard) view;
+			sampleCard.bind(cursor);
+		}
+		return;
+
+	}
+
+	@Override
+	public ClickToOpenCloseLayout setClickToOpenCloseLayout(View view) {
+
+		// return ClickToOpenCloseLayout from your layout by using findViewById
+		return ((ClickToOpenCloseLayout) view.findViewById(R.id.main_container));
+	}
+
+	@Override
+	public ExpandAndShrinkCardView setExpandAndSrinkView(View view) {
+
+		// return ExpandAndShrinkCardView from your layout by using findViewById
+		return ((ExpandAndShrinkCardView) view.findViewById(R.id.root_view));
+	}
 }
